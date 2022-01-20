7a5bf545f2d57d1bd1eee54bf045121263b812ad
Juan Rodriguez <juanprodrigues.33@gmail.com>
2022-01-20 15:40:10 -0300

    Juan:entidad rol creada

---
+++ src/main/java/com/alkemy/ong/entity/RolEntity.java
@@ -0,0 +1,29 @@
+package com.alkemy.ong.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "roles")
+@Setter
+@Getter
+public class RolEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    private LocalDateTime date;
+
+}
