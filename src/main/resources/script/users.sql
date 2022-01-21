d34ce964846585918648f289618e3042c1607a0c
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-01-21 17:32:38 -0300

    Attribute deleted changed by softDelete in the script sql.

--- src/main/resources/script/users.sql
+++ src/main/resources/script/users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users(
-	id INTEGER,
+	id VARCHAR(255),
 	first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE users(
     photo VARCHAR(100) NULL,
     role_id INTEGER,
     timestamps TIMESTAMP,
-    deleted BIT NOT NULL DEFAULT 0,
+    softDelete BIT NOT NULL DEFAULT 0,
     CONSTRAINT pk_users PRIMARY KEY(id),
 	CONSTRAINT fk_user_role FOREIGN KEY(role_id) REFERENCES Roles(id)
 );
