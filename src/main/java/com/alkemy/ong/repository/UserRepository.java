c0b7402a3b0e162434b4d175e11d672cb3a9bf71
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-01-21 17:31:18 -0300

    Id type modified.

--- src/main/java/com/alkemy/ong/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.alkemy.ong.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, String>{
 
 }
