19e833cc4f9312ec4ab6656c734c4e47999bb3a3
Muriel3000 <murielmartinacorrea@gmail.com>
2022-01-24 09:49:55 -0300

    Category id type modified in Repository

--- src/main/java/com/alkemy/ong/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer>{
+public interface CategoryRepository extends JpaRepository<Category, String>{
     public Category findByCategoryId(String catId);
 }
