6c78062541238c6da0fb60689ab40a69415411a2
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-01-24 14:45:57 -0300

    feat: agrego entidad member

---
+++ src/main/java/com/alkemy/ong/model/member/Member.java
@@ -0,0 +1,37 @@
+package com.alkemy.ong.model.member;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import com.alkemy.ong.model.PersistentEntity;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.hibernate.annotations.SQLDelete;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "members")
+@Getter
+@Setter
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE members SET softDelete = true WHERE id=?")
+@FilterDef(name = "deletedMemberFilter",
+        parameters = @ParamDef(name = "isDeleted", type = "boolean"))
+@Filter(name = "deletedMemberFilter", condition = "softDelete = :isDeleted")
+public class Member extends PersistentEntity {
+
+    @NotNull(message = "El nombre no puede ser nulo")
+    @Column(nullable = false)
+    private String name;
+    private String facebookUrl;
+    private String instagramUrl;
+    private String linkedinUrl;
+    @NotNull(message = "La imagen no puede ser nula")
+    @Column(nullable = false)
+    private String image;
+    private String description;
+}
