e03a28ee39c61f7fa5792d264969d550b16f49d2
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-01-26 15:50:10 -0300

    refactor(category): utiliza persistent entity y ajusto annotations

--- src/main/java/com/alkemy/ong/entity/Category.java
+++ src/main/java/com/alkemy/ong/entity/Category.java
@@ -1,73 +1,48 @@
 package com.alkemy.ong.entity;
 
-import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.*;
-
-import java.time.LocalDateTime;
-import java.util.*;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.hibernate.annotations.SQLDelete;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "categories")
-@SQLDelete(sql = "UPDATE categories SET deleted = true WHERE category_id=?")
-@Where(clause = "deleted=false")
-@FilterDef(name = "deletedCategoryFilter", parameters = @ParamDef(name = "deleted", type = "boolean"))
-@Filter(name = "deletedCategoryFilter", condition = "deleted = :isDeleted")
-public class Category {
-    
-    @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
-    @Column(name = "category_id")
-    private String categoryId; 
+@SQLDelete(sql = "UPDATE categories SET soft_delete = true WHERE id=?")
+@FilterDef(name = "deletedCategoryFilter",
+        parameters = @ParamDef(name = "isDeleted", type = "boolean"))
+@Filter(name = "deletedCategoryFilter", condition = "soft_delete = :isDeleted")
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category extends PersistentEntity {
 
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "image")
     private String image;
-    private boolean softDelete = false;
-    private LocalDateTime timestamps;
 
+    // ! Esto tiene que existir? porque también lo mapea news. medio quilombo bidireccionales
     @OneToMany(mappedBy = "categoryId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "new_id")
     private List<News> news = new ArrayList<>();
-
-    public String getId(){
-        return categoryId;
-    }
-     
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name1){
-        this.name = name1;
-    }
-
-    public String getDescription(){
-        return description;
-    }
-
-    public void setDescription(String d){
-        this.description = d;
-    }
-
-    public String getImage(){
-        return image;
-    }
-
-    public void setImage(String im){
-        this.image = im;
-    }
-
-    public List<News> getNews(){
-        return news;
-    }
-
-    public void setNews(List<News> newsList){
-        this.news = newsList;
-    } 
-    
 
 }
