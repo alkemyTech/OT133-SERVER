eaaa0d3847f193b2d8757fc7f25a90ac847be988
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-01-27 12:26:22 -0300

    refactor(auth): UserDAO

--- src/main/java/com/alkemy/ong/controller/AuthController.java
+++ src/main/java/com/alkemy/ong/controller/AuthController.java
@@ -1,85 +1,56 @@
 package com.alkemy.ong.controller;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
-import com.alkemy.ong.security.UserDetailServiceImpl;
+import com.alkemy.ong.entity.User;
+import com.alkemy.ong.security.exception.UserAlreadyExistsException;
+import com.alkemy.ong.security.payload.SignupRequest;
+import com.alkemy.ong.service.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 @RestController
-@RequestMapping("/auth")
-public class AuthController {
+@RequestMapping("auth")
+public class AuthController extends BaseController {
 
 	@Autowired
-	private AuthenticationManager authenticationManager;
+	private UserDAO userService;
 
-	@Autowired
-	private UserDetailServiceImpl userDetails;
+	@PostMapping(path = "register", produces = "application/json")
+	public ResponseEntity<?> registerUser(@Validated @RequestBody SignupRequest signupRequest)
+			throws UserAlreadyExistsException {
 
-	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+		// Creacion de nuevo usuario
+		User user = new User();
+		user.setFirstName(signupRequest.getFirstname());
+		user.setLastName(signupRequest.getLastname());
+		user.setEmail(signupRequest.getEmail());
+		user.setPassword(signupRequest.getPassword());
 
-	@PostMapping("/login")
-	public ResponseEntity<?> loginUser(@RequestParam String email, @RequestParam String password) {
-		Map<String, Object> response = new HashMap<>();
-		boolean authenticated = false;
+		// Creacion de la URI
+		URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath()
+				.path("/auth/register").toUriString());
 
-		try {
-			this.authenticationManager
-					.authenticate(new UsernamePasswordAuthenticationToken(email, password));
-			UserDetails userDetails = this.userDetails.loadUserByUsername(email);
-
-			if (userDetails != null) {
-				authenticated = this.passwordEncoder.matches(password, userDetails.getPassword());
-				if (authenticated) {
-					response.put("ok", userDetails);
-					return new ResponseEntity<>(response, HttpStatus.OK);
-				}
-			} else {
-				response.put("Forbidden", HttpStatus.FORBIDDEN);
-				return ResponseEntity.badRequest().body(response);
-			}
-		} catch (BadCredentialsException e) {
-			response.put("Forbidden", HttpStatus.FORBIDDEN);
-			return ResponseEntity.badRequest().body(response);
-		}
-		response.put("ok", Boolean.FALSE);
-		return ResponseEntity.badRequest().body(response);
+		return ResponseEntity.created(uri).body(userService.create(user));
 	}
 
-	// @PostMapping("/register")
-	// public ResponseEntity<?> registerUser(@RequestBody SignupRequest signupRequest){
-
-	// //verificacion del mail
-	// if(userRepository.existsByEmail(signupRequest.getEmail())){
-	// return ResponseEntity
-	// .badRequest()
-	// .body("Error: Email is already in use!");
-	// }
-
-	// //Creacion de nuevo usuario
-	// User user = new User();
-	// user.setFirstName(signupRequest.getFirstname());
-	// user.setLastName(signupRequest.getLastname());
-	// user.setEmail(signupRequest.getEmail());
-	// user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
-
-	// Rol userRole = rolRepository.findByName(Roles.ROL_USER);
-	// user.setRoleId(userRole);
-	// userRepository.save(user);
-
-	// return new ResponseEntity<String>("User registered successfully", HttpStatus.OK);
-	// }
+	@ResponseStatus(HttpStatus.CONFLICT)
+	@ExceptionHandler(UserAlreadyExistsException.class)
+	public Map<String, String> handleUserExistExceptions() {
+		Map<String, String> errors = new HashMap<>();
+		errors.put("mail", "The indicated email address is already in use");
+		return errors;
+	}
 
 }
--- src/main/java/com/alkemy/ong/service/UserDAO.java
+++ src/main/java/com/alkemy/ong/service/UserDAO.java
@@ -1,20 +1,63 @@
 package com.alkemy.ong.service;
 
 import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import com.alkemy.ong.entity.Rol;
 import com.alkemy.ong.entity.User;
+import com.alkemy.ong.enums.Roles;
+import com.alkemy.ong.repository.RolRepository;
 import com.alkemy.ong.repository.UserRepository;
+import com.alkemy.ong.security.exception.UserAlreadyExistsException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserDAO {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	public Optional<User> getByEmail(String email){
+
+	@Autowired
+	private RolRepository rolRepository;
+
+	@Autowired
+	private PasswordEncoder pwEncoder;
+
+	// --------------------------------------------------------------------------------------------
+	// Get
+	// --------------------------------------------------------------------------------------------
+
+	public Optional<User> getByEmail(String email) {
 		return this.userRepository.findByEmail(email);
 	}
+
+	// --------------------------------------------------------------------------------------------
+	// Create
+	// --------------------------------------------------------------------------------------------
+
+	public User create(User user) throws UserAlreadyExistsException {
+
+		if (emailExists(user.getEmail())) {
+			throw new UserAlreadyExistsException(
+					"The email " + user.getEmail() + " already exists already in use");
+		}
+
+		Rol rol = rolRepository.findByName(Roles.ROL_USER);
+
+		user.setPassword(pwEncoder.encode(user.getPassword()));
+		user.setRoleId(rol);
+
+		User created = userRepository.save(user);
+
+		return created;
+	}
+
+	// --------------------------------------------------------------------------------------------
+	// Internal Methods
+	// --------------------------------------------------------------------------------------------
+
+	private boolean emailExists(String email) {
+		return userRepository.findByEmail(email).isPresent();
+	}
+
 }
