1859f362defef8b89969750c19e4ec1896ce7e6b
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-01-28 18:52:11 -0300

    Import added.

--- src/main/java/com/alkemy/ong/controller/NewController.java
+++ src/main/java/com/alkemy/ong/controller/NewController.java
@@ -5,6 +5,7 @@ import com.alkemy.ong.service.NewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
