7c4fd185b40e61b25ecb1fc2948760d5beb934c9
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-01-28 20:39:22 -0300

    Endpoint login added.It was deleted.

--- src/main/java/com/alkemy/ong/controller/AuthController.java
+++ src/main/java/com/alkemy/ong/controller/AuthController.java
@@ -25,11 +25,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -44,6 +46,43 @@ public class AuthController extends BaseController {
 	
 	@Autowired
 	private UserMapper userMapper;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private UserDetailServiceImpl userDetails;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	
+	//Este método debe estar según la OT133-25
+	@PostMapping("/login")
+	public ResponseEntity<?> loginUser(@RequestParam String email,@RequestParam String password){
+		Map<String,Object> response = new HashMap<>();
+		boolean authenticated= false;
+
+		try {
+			this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+			UserDetails userDetails = this.userDetails.loadUserByUsername(email);
+			
+			if(userDetails != null) {
+				authenticated = this.passwordEncoder.matches(password, userDetails.getPassword());
+				if(authenticated) {
+					response.put("ok", userDetails);
+					return new ResponseEntity<>(response,HttpStatus.OK);
+				}
+			}else {
+				response.put("Forbidden", HttpStatus.FORBIDDEN);
+			}
+			
+		}catch(BadCredentialsException e) {
+			response.put("Forbidden", HttpStatus.FORBIDDEN);
+		}
+		return new ResponseEntity<>(response,HttpStatus.FORBIDDEN);
+	}
+	
   
 	@PostMapping(path = "register", produces = "application/json")
 	public ResponseEntity<?> registerUser(@Validated @RequestBody SignupRequest signupRequest)
