3ccb1b039092faf21ef537736e9f0b8faf5f993c
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-01-28 19:07:59 -0300

    Attributes added.

--- src/main/java/com/alkemy/ong/dto/UserDTO.java
+++ src/main/java/com/alkemy/ong/dto/UserDTO.java
@@ -1,5 +1,9 @@
 package com.alkemy.ong.dto;
 
+import java.util.List;
+
+import com.alkemy.ong.entity.Rol;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +13,13 @@ import lombok.NoArgsConstructor;
 @Data
 public class UserDTO {
 
+	private String id;
 	private String firstName;
 	private String lastName;
 	private String email;
+	private String photo;
+	private String password;
+	private List<Rol> roles;
+	
 	
 }
