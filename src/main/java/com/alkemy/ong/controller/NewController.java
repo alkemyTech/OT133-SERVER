411076db7fb5b8b124771345baa48b964bba814c
Agus1097 <agustinleyes10@gmail.com>
2022-01-28 13:23:42 -0300

    Post endpoint news

---
+++ src/main/java/com/alkemy/ong/controller/NewController.java
@@ -0,0 +1,23 @@
+package com.alkemy.ong.controller;
+
+import com.alkemy.ong.dto.NewDTO;
+import com.alkemy.ong.service.NewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("news")
+public class NewController {
+
+    @Autowired
+    NewService newService;
+
+    @PostMapping
+    public ResponseEntity<NewDTO> save(NewDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(newService.save(dto));
+    }
+}
