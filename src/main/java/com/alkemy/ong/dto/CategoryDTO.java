af41035a5be5ae8fd7ee98ca33634a99f84ee583
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-01-30 17:33:08 -0300

    Validations annotations added.

--- src/main/java/com/alkemy/ong/dto/CategoryDTO.java
+++ src/main/java/com/alkemy/ong/dto/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.dto;
 
+import javax.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,8 +10,10 @@ import lombok.ToString;
 @ToString
 public class CategoryDTO {
 
+	@NotEmpty(message = "no es un valor admitido")
     private String name;
 
+	@NotEmpty(message = "no es un valor admitido")
     private String description;
 
     private String image;
