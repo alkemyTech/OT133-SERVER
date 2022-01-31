a7d0127413a530c942dfe83ef0fe67ba8a47b587
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-01-31 20:45:00 -0300

    Get by ID method added.

--- src/main/java/com/alkemy/ong/service/NewService.java
+++ src/main/java/com/alkemy/ong/service/NewService.java
@@ -1,7 +1,11 @@
 package com.alkemy.ong.service;
 
+import java.util.Optional;
+
 import com.alkemy.ong.dto.NewDTO;
 
 public interface NewService {
     NewDTO save(NewDTO dto);
+    
+    Optional<NewDTO> getById(String id);
 }
--- src/main/java/com/alkemy/ong/service/impl/NewServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/NewServiceImpl.java
@@ -5,23 +5,36 @@ import com.alkemy.ong.entity.News;
 import com.alkemy.ong.mapper.NewMapper;
 import com.alkemy.ong.repository.NewsRepository;
 import com.alkemy.ong.service.NewService;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NewServiceImpl implements NewService {
 
-  @Autowired
-  NewMapper newMapper;
+	@Autowired
+	NewMapper newMapper;
 
-  @Autowired
-  NewsRepository newsRepository;
+	@Autowired
+	NewsRepository newsRepository;
 
-  public NewDTO save(NewDTO dto) {
-    News news = newMapper.newDTO2Entity(dto);
-    News newSave = newsRepository.save(news);
-    NewDTO result = newMapper.entity2newDTO(newSave);
-    return result;
+	public NewDTO save(NewDTO dto) {
+		News news = newMapper.newDTO2Entity(dto);
+		News newSave = newsRepository.save(news);
+		NewDTO result = newMapper.entity2newDTO(newSave);
+		return result;
 
-  }
+	}
+
+	@Override
+	public Optional<NewDTO> getById(String id) {
+		Optional<News> optNew = this.newsRepository.findById(id);
+		if(!optNew.isPresent()) {
+			return Optional.empty();
+		}else {
+			return Optional.of(this.newMapper.entity2newDTO(optNew.get()));
+		}
+	}
 }
