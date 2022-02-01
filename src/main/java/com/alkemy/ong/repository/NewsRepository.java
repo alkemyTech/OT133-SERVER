2f7e920339668a73bda19e3d926412c9815f3a92
maurodell <delloliomauro@gmail.com>
2022-01-31 23:53:19 -0300

    ticket 47: update news and ticket 48: delete news

--- src/main/java/com/alkemy/ong/repository/NewsRepository.java
+++ src/main/java/com/alkemy/ong/repository/NewsRepository.java
@@ -1,10 +1,12 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.entity.News;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NewsRepository extends JpaRepository<News, Integer>{
+public interface NewsRepository extends JpaRepository<News, String>{
     
 }
