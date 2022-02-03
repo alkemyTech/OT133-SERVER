abda2d2f607c4e34ee0d72d169c18e586195623d
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-02-03 13:19:46 -0300

    Conflict solved

--- src/main/java/com/alkemy/ong/controller/AuthController.java
+++ src/main/java/com/alkemy/ong/controller/AuthController.java
@@ -16,6 +16,9 @@ import com.alkemy.ong.service.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +26,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -70,4 +74,32 @@ public class AuthController extends BaseController {
     errors.put("mail", "The indicated email address is already in use");
     return errors;
   }
+  
+  
+//Este método debe estar según la OT133-25
+	@PostMapping("/login")
+	public ResponseEntity<?> loginUser(@RequestParam String email, @RequestParam String password) {
+		Map<String, Object> response = new HashMap<>();
+		boolean authenticated = false;
+
+		try {
+			this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+			UserDetails userDetails = this.userDetails.loadUserByUsername(email);
+
+			if (userDetails != null) {
+				authenticated = this.passwordEncoder.matches(password, userDetails.getPassword());
+				if (authenticated) {
+					response.put("ok", userDetails);
+					return new ResponseEntity<>(response, HttpStatus.OK);
+				}
+			} else {
+				response.put("Forbidden", HttpStatus.FORBIDDEN);
+			}
+
+		} catch (BadCredentialsException e) {
+			response.put("Forbidden", HttpStatus.FORBIDDEN);
+		}
+
+		return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+	}
 }
