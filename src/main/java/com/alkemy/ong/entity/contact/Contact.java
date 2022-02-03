f16794024e5470dc19ca1ef6eabd825fd945cdc1
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-02-03 13:46:53 -0300

    feat(organization): contactos y mapeos

---
+++ src/main/java/com/alkemy/ong/entity/contact/Contact.java
@@ -0,0 +1,23 @@
+package com.alkemy.ong.entity.contact;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class Contact {
+
+  @Column(name = "facebook_url")
+  private String facebookUrl;
+
+  @Column(name = "linkedin_url")
+  private String linkedinUrl;
+
+  @Column(name = "instagram_url")
+  private String instagramUrl;
+}
