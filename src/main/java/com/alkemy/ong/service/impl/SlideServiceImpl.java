07dceb956c5bac0fa7cd6ab9195282505e64fc85
Muriel3000 <murielmartinacorrea@gmail.com>
2022-02-03 20:38:48 -0300

    Endpoint DELETE/slides/id developed

--- src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
@@ -1,12 +1,31 @@
 package com.alkemy.ong.service.impl;
 
+import java.util.Optional;
+
+import com.alkemy.ong.entity.Slide;
 import com.alkemy.ong.repository.SlideRepository;
 import com.alkemy.ong.service.SlideService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SlideServiceImpl implements SlideService {
     
     @Autowired
     private SlideRepository slideRepository;
+
+    public Slide findById(String id){
+        Optional<Slide> slide = slideRepository.findById(id);
+		if(slide.isPresent())
+            return slide.get();
+        return null;
+    }
+
+    public void deleteSlide(String id) throws Exception{
+        Slide slide = this.findById(id);
+        if(slide != null){
+            slideRepository.delete(slide);
+        } else throw new Exception("The specified slide does not exist");
+    }
 }
