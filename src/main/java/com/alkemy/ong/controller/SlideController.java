21cafce6c790711c3e82d546b7e64a44b9c0a6c4
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-02-04 14:08:11 -0300

    Endpoint update added.

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -10,6 +10,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.alkemy.ong.service.SlideService;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +37,7 @@ public class SlideController {
     
     @Autowired
     private SlideService slideService;
+
 
     @PreAuthorize("hasAuthority('ROL_ADMIN')")
     @DeleteMapping("/{id}")
@@ -32,5 +51,21 @@ public class SlideController {
             response.put("Exception: " + e.getLocalizedMessage(), HttpStatus.SC_CONFLICT);
             return ResponseEntity.ok(response);
         }
+    
+    
+    @PreAuthorize("hasAuthority('ROL_ADMIN')")
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateSlide(@Validated @RequestBody SlideDTO slideDTO, @PathVariable UUID id){
+    	Map<String, Object> response = new HashMap<>();
+    	
+    	Optional<SlideDTO> slideUpdateDTO = this.slideService.update(slideDTO,id.toString());
+    	
+    	if(!slideUpdateDTO.isPresent()) {
+    		response.put("Error", String.format("Slide with ID %s not found.", id));
+			return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+    	}
+    	response.put("ok", slideUpdateDTO.get());
+		return ResponseEntity.ok(response);
+
     }
 }
