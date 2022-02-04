73b567f89ca1a69b81599c10e5c9cdebdadb2a30
Muriel3000 <murielmartinacorrea@gmail.com>
2022-02-04 09:08:45 -0300

    Method findById deleted

--- src/main/java/com/alkemy/ong/service/SlideService.java
+++ src/main/java/com/alkemy/ong/service/SlideService.java
@@ -3,8 +3,6 @@ package com.alkemy.ong.service;
 import com.alkemy.ong.entity.Slide;
 
 public interface SlideService {
-    
-    Slide findById(String id);
 
     void deleteSlide(String id) throws Exception;
 }
--- src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
@@ -15,16 +15,10 @@ public class SlideServiceImpl implements SlideService {
     @Autowired
     private SlideRepository slideRepository;
 
-    public Slide findById(String id){
-        Optional<Slide> slide = slideRepository.findById(id);
-		if(slide.isPresent())
-            return slide.get();
-        return null;
-    }
-
     public void deleteSlide(String id) throws Exception{
-        Slide slide = this.findById(id);
-        if(slide != null){
+        Optional<Slide> slideOptional = slideRepository.findById(id);
+        if(slideOptional.isPresent()){
+            Slide slide = slideOptional.get();
             slideRepository.delete(slide);
         } else throw new Exception("The specified slide does not exist");
     }
