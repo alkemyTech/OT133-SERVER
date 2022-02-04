9de1295df8f932482a0b0b6212be9a56b3cf0a0e
Muriel3000 <murielmartinacorrea@gmail.com>
2022-02-04 16:56:48 -0300

    @Validated notation added to PUT/activities/id, and constraints added to activityDTO

--- src/main/java/com/alkemy/ong/controller/ActivityController.java
+++ src/main/java/com/alkemy/ong/controller/ActivityController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -35,7 +36,7 @@ public class ActivityController {
 
 	@PreAuthorize("hasAuthority('ROL_ADMIN')")
 	@PutMapping("/{id}")
-	public ResponseEntity<?> updateActivity(@PathVariable String id, @RequestBody ActivityDTO activity){
+	public ResponseEntity<?> updateActivity(@PathVariable String id, @Validated @RequestBody ActivityDTO activity){
 		try{
 			if(activityService.activityExists(id)){ 
 				activityService.validateActivityForUpdate(activity);
