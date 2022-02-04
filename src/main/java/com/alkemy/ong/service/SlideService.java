1c94eac0e76740f11747e91e59fb854b2696d0e9
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-02-04 14:10:44 -0300

    Slide features added.

--- src/main/java/com/alkemy/ong/service/SlideService.java
+++ src/main/java/com/alkemy/ong/service/SlideService.java
@@ -1,8 +1,17 @@
 package com.alkemy.ong.service;
 
+import java.util.Optional;
+import com.alkemy.ong.entity.Slide;
+
+import com.alkemy.ong.dto.SlideDTO;
 import com.alkemy.ong.entity.Slide;
 
 public interface SlideService {
+    
+	Optional<Slide> getbyId(String id);
+	
+	Optional<SlideDTO> update(SlideDTO slideDTO,String id);
+	
+	void deleteSlide(String id) throws Exception;
 
-    void deleteSlide(String id) throws Exception;
 }
