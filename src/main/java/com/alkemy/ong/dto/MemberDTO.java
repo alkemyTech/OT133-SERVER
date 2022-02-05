370d4a20283a241d74e04940b53b9dd2f2ea4076
maurodell <delloliomauro@gmail.com>
2022-02-05 16:15:19 -0300

    refactor 05-02-22

--- src/main/java/com/alkemy/ong/dto/MemberDTO.java
+++ src/main/java/com/alkemy/ong/dto/MemberDTO.java
@@ -2,10 +2,13 @@ package com.alkemy.ong.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MemberDTO {
   
   @NotBlank(message = "El nombre es obligatorio")
