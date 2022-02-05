04e85aa224e0f1d79871508342f512f9fea3975f
maurodell <delloliomauro@gmail.com>
2022-02-05 16:49:54 -0300

    delete constructor

--- src/main/java/com/alkemy/ong/dto/MemberDTO.java
+++ src/main/java/com/alkemy/ong/dto/MemberDTO.java
@@ -1,14 +1,10 @@
 package com.alkemy.ong.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class MemberDTO {
   
   @NotBlank(message = "El nombre es obligatorio")
