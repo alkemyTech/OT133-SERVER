502c2a25db7ee3f11811c23445636fefe36f9a88
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-02-07 11:31:25 -0300

    ContactService added.One method added.

--- src/main/java/com/alkemy/ong/service/ContactService.java
+++ src/main/java/com/alkemy/ong/service/ContactService.java
@@ -1,5 +1,11 @@
 package com.alkemy.ong.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.alkemy.ong.dto.ContactDTO;
+
 public interface ContactService {
 
+	Optional<List<ContactDTO>> getAll();
 }
