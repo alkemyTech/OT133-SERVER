b80bb1e0f274f11f5e3041d406341a30f41c8808
Muriel3000 <murielmartinacorrea@gmail.com>
2022-02-07 11:21:46 -0300

    Endpoint POST /contacts

---
+++ src/main/java/com/alkemy/ong/exception/ContactException.java
@@ -0,0 +1,7 @@
+package com.alkemy.ong.exception;
+
+public class ContactException extends Exception{
+    public ContactException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/alkemy/ong/service/ContactService.java
+++ src/main/java/com/alkemy/ong/service/ContactService.java
@@ -1,5 +1,8 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.dto.ContactDTO;
+
 public interface ContactService {
 
+    ContactDTO save(ContactDTO contactDTO);
 }
