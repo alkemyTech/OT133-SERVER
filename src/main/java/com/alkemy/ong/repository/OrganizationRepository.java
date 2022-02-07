e1db5b1b1a36a05eeae2ba9676b5227fc079c9e9
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-02-07 17:02:59 -0300

    fix(OT133-64): improved query

--- src/main/java/com/alkemy/ong/repository/OrganizationRepository.java
+++ src/main/java/com/alkemy/ong/repository/OrganizationRepository.java
@@ -9,15 +9,14 @@ import com.alkemy.ong.entity.Organization;
 import java.util.List;
 
 @Repository
-public interface OrganizationRepository extends JpaRepository<Organization, String>{
-	
-    
-	@Query("SELECT p.name, p.image, p.phone, p.address, p.contact.facebookUrl, p.contact.linkedinUrl, p.contact.instagramUrl FROM Organization p")
+public interface OrganizationRepository extends JpaRepository<Organization, String> {
 
-	public Iterable<Organization> readAllDefined();
-	
-	public boolean existsByEmail(String email);
-  
+
+  @Query("SELECT p.name, p.image, p.phone, p.address, p.contact FROM Organization p join p.contact")
+  public Iterable<Organization> readAllDefined();
+
+  public boolean existsByEmail(String email);
+
   @Query("SELECT o FROM Organization o ORDER BY o.id DESC")
   List<Organization> findTopById();
 
