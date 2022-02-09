d87da0708f42f08bf6f5d98453ece0b239736480
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-02-09 16:17:37 -0300

    feat(OT133-83): Agregar paginación a Testimonios

--- src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
+++ src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TestimonialRepository extends JpaRepository<Testimonial, String> {
+
 }
--- src/main/java/com/alkemy/ong/service/TestimonialService.java
+++ src/main/java/com/alkemy/ong/service/TestimonialService.java
@@ -1,9 +1,12 @@
 package com.alkemy.ong.service;
 
+import java.util.List;
 import com.alkemy.ong.dto.TestimonialDTO;
 import com.alkemy.ong.dto.TestimonialIDDTO;
 
 public interface TestimonialService {
+
+  List<TestimonialDTO> read(Integer page);
 
   TestimonialIDDTO create(TestimonialDTO dto);
 
