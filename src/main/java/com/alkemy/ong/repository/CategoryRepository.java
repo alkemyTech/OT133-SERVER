d6a314b22884835c1c9540c0633631abb6f78ce2
maurodell <delloliomauro@gmail.com>
2022-02-10 20:42:50 -0300

    Query to sort Object

--- src/main/java/com/alkemy/ong/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -2,10 +2,17 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.entity.Category;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, String> {
-  public Category findByName(String name);
+	public Category findByName(String name);
+	
+	@Query("SELECT s from Category s ORDER BY s.timestamps ASC")
+	public Page<Category> findAll(Pageable pageable);
+	
 }
