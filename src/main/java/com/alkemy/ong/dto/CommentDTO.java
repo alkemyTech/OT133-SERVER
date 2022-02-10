de736785fdbd62e66bbd628871ec2c38393eff83
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-02-10 12:57:03 -0300

    Class DTO added.

---
+++ src/main/java/com/alkemy/ong/dto/CommentDTO.java
@@ -0,0 +1,24 @@
+package com.alkemy.ong.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CommentDTO {
+
+	@NotBlank(message = "No es un valor aceptable")
+	private String userId;
+	
+	@NotBlank(message = "No es un valor aceptable")
+	private String body;
+	
+	@NotBlank(message = "No es un valor aceptable")
+	private String newId;
+}
