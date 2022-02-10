050071508af4fcdbf67c2863d9e4636e1f5b9833
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-02-10 10:57:08 -0300

    docs(swagger): API Info

--- src/main/java/com/alkemy/ong/config/SpringFoxConfig.java
+++ src/main/java/com/alkemy/ong/config/SpringFoxConfig.java
@@ -1,9 +1,11 @@
 package com.alkemy.ong.config;
 
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -13,7 +15,12 @@ public class SpringFoxConfig {
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any()).build();
+        .paths(PathSelectors.any()).build().apiInfo(apiInfo());
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfo("OT133-Server", "Alkemy 2022", "v1", "urn:tos", null, "MIT LICENSE",
+        "https://mit-license.org/", Collections.emptyList());
   }
 
 
