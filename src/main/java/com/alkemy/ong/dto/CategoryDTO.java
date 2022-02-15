e7ecb74ba03620b285cbf02642ffdd6987553379
CaroRodrigo <rodrigo_caro_cba@hotmail.com>
2022-02-15 00:04:25 -0300

    OT133-85 Agrege @Schema() en CategoryDTO

--- src/main/java/com/alkemy/ong/dto/CategoryDTO.java
+++ src/main/java/com/alkemy/ong/dto/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -10,19 +11,25 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Schema(description = "CategoryDTO")
 public class CategoryDTO {
 
-	@NotBlank(message = "no es un valor valido.")
-	@Size(message = "debe tene minimo 4 caracteres", min = 4)
+    @Schema(description = "Category name",required = true,example = "Family.")
+    @NotBlank(message = "no es un valor valido.")
+    @Size(message = "debe tene minimo 4 caracteres", min = 4)
     private String name;
 
-	@NotBlank(message = "no es un valor valido.")
-	@Size(message = "debe tene minimo 5 caracteres", min = 5)
+    @Schema(description = "Category description",required = true,example = "This category is about family.")
+    @NotBlank(message = "no es un valor valido.")
+    @Size(message = "debe tene minimo 5 caracteres", min = 5)
     private String description;
 
+    @Schema(description = "Category image.",required = false)
     private String image;
 
+    @Schema(description = "Category Soft delete.",required = false,example = "Do not modify this field." )
     private boolean softDelete;
 
+    @Schema(description = "Member image.",required = false,example = "Do not modify this field.")
     private String timestamps;
 }
