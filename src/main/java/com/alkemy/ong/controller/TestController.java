5a9018a457210ef4b130232c78cf179e917272ff
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-02-15 11:03:05 -0300

    refactor: api ignore

--- src/main/java/com/alkemy/ong/controller/TestController.java
+++ src/main/java/com/alkemy/ong/controller/TestController.java
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-
+import springfox.documentation.annotations.ApiIgnore;
 import com.alkemy.ong.service.images.ImageUploaderService;
 
 import java.io.File;
@@ -21,45 +20,48 @@ import java.util.Base64;
 import org.apache.commons.io.FileUtils;
 
 @RestController
+@ApiIgnore
 @RequestMapping("/auth/test")
 public class TestController {
-	@Value("${cloud.aws.credentials.bucketName}")
-	private String bucketName;
-	@Autowired
-	ImageUploaderService imageUploader;
+  @Value("${cloud.aws.credentials.bucketName}")
+  private String bucketName;
+  @Autowired
+  ImageUploaderService imageUploader;
 
-	@GetMapping("/all")
-	public String freeAccess() {
-		return "Publico";
-	}
+  @GetMapping("/all")
+  public String freeAccess() {
+    return "Publico";
+  }
 
-	@GetMapping("/user")
-	@PreAuthorize("hasRole('ROL_USER')")
-	public String userAccess() {
-		return "User";
-	}
+  @GetMapping("/user")
+  @PreAuthorize("hasRole('ROL_USER')")
+  public String userAccess() {
+    return "User";
+  }
 
-	@GetMapping("/admin")
-	@PreAuthorize("hasRole('ROL_ADMIN')")
-	public String adminAccess() {
-		return "Admin";
-	}
+  @GetMapping("/admin")
+  @PreAuthorize("hasRole('ROL_ADMIN')")
+  public String adminAccess() {
+    return "Admin";
+  }
 
-	// /auth/test/upload
-	@PostMapping("/upload")
-	public ResponseEntity<?> uploadFile(@RequestPart(value = "file") MultipartFile file) throws IOException {
+  // /auth/test/upload
+  @PostMapping("/upload")
+  public ResponseEntity<?> uploadFile(@RequestPart(value = "file") MultipartFile file)
+      throws IOException {
 
-		String response = imageUploader.uploadImage(file);
+    String response = imageUploader.uploadImage(file);
 
-		return ResponseEntity.status(HttpStatus.OK).body(response);
-	}
+    return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
 
-	@PostMapping("/converse")
-	public ResponseEntity<?> converseFile(@RequestPart(value = "file") String file) throws IOException {
-		
-		byte[] fileContent = FileUtils.readFileToByteArray(new File(file));
-		String encodedString = Base64.getEncoder().encodeToString(fileContent);
-		
-		return ResponseEntity.status(HttpStatus.OK).body(encodedString);
-	}
+  @PostMapping("/converse")
+  public ResponseEntity<?> converseFile(@RequestPart(value = "file") String file)
+      throws IOException {
+
+    byte[] fileContent = FileUtils.readFileToByteArray(new File(file));
+    String encodedString = Base64.getEncoder().encodeToString(fileContent);
+
+    return ResponseEntity.status(HttpStatus.OK).body(encodedString);
+  }
 }
