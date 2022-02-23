aab2f8ffdc24752ac9881d054888c9ee54c847e2
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-02-23 19:45:52 -0300

    perf: query

--- src/main/java/com/alkemy/ong/repository/MemberRepository.java
+++ src/main/java/com/alkemy/ong/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.repository;
 import java.util.List;
 import com.alkemy.ong.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
 
@@ -12,4 +13,8 @@ public interface MemberRepository extends JpaRepository<Member, String> {
   // Metodo creado ya que si no genera error y no corre el proyecto
 
   public Member findByName(String name);
+
+  @Query("SELECT M.name, M.socialLinks FROM Member M JOIN M.socialLinks")
+  public List<Member> findSocialLinksForAll();
+
 }
